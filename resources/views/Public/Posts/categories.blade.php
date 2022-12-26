e46a3856f62e13f7fa5cbfe0f0d5046fe1c0a2fc
derlisrd <derlisruizdiaz@hotmail.com>
2022-12-26 17:39:18 -0300

    blog

--- resources/views/Public/Posts/categories.blade.php
+++ resources/views/Public/Posts/categories.blade.php
@@ -27,32 +27,31 @@
 
 @section('postcontent')
 
-<div class="container">
+<div class="container mb-5">
     <div class="row g-5">
         @foreach ($posts as $post)
         <div class="col-lg-4">
             <div class="post-entry d-block small-post-entry-v">
-                <div class="thumbnail">
-                    @if($post->image_id)
+                @if($post->image_id)
+                <div class="thumbnail p-1 border rounded">
                     <a href="{{ route('public.post',$post->slug) }}">
                         <img src="{{ $post->images->url }}" alt="Image" class="img-fluid">
                     </a>
-                    @endif
                 </div>
+                @endif
                 <div class="content">
-                    <div class="post-meta mb-1">
-                        <a href="{{ route('public.category',$post->category->slug) }}" class="category"> {{ $post->category->title }} </a> -
-                        <span class="date">{{ $post->created_at->format('d-m-Y') }}</span>
+                    <div class="post-meta mb-1 text-muted">
+                        <a href="{{ route('public.category',$post->category->slug) }}" class="category"> {{ $post->category->title }} </a>  —
+                        <span class="date font-weight-light">{{ $post->created_at->format('d-m-Y') }}</span>
                     </div>
-                    <h2 class="heading mb-3"><a href="{{ route('public.post',$post->slug) }}">{{ $post->title }}</a></h2>
-                    <p>{{ $post->description }}</p>
+                    <h2 class="heading my-2"><a href="{{ route('public.post',$post->slug) }}">{{ $post->title }}</a></h2>
+                    <p class="text-muted font-weight-light subheading">{{ $post->description }}</p>
                     <a href="{{ route('public.post',$post->slug) }}" class="post-author d-flex align-items-center">
                         <div class="author-pic">
                             {{-- <img src="images/person_1.jpg" alt="Image"> --}}
                         </div>
-                        <div class="text">
-                            <strong>{{ $post->author->name }}</strong>
-                            <span>Editor</span>
+                        <div>
+                            <small class="font-weight-light">Por: {{ $post->author->name }}</small>
                         </div>
                     </a>
                 </div>
--- resources/views/Public/Posts/post.blade.php
+++ resources/views/Public/Posts/post.blade.php
@@ -37,7 +37,7 @@
 
 
 @section('postcontent')
-<article class="mb-4">
+<article class="mb-4 article-content">
     <div class="container px-4 px-lg-5">
         <div class="row gx-4 gx-lg-5 justify-content-center">
             <div class="col-md-10 col-lg-8 col-xl-7">
@@ -47,23 +47,26 @@
     </div>
 </article>
 
-    <div class="my-5 container-sm card p-3">
+    <div class="my-5 container-sm p-3 rounded border">
         <form method="post" action="#" >
             @csrf
             {{-- {!! htmlFormSnippet() !!} --}}
-            <div class="row">
+            <div class="row gap-3">
                 <div class="col-12">
                     <h3 class="mb-4">Deja tu comentario: </h3>
                 </div>
                 <div class="col-12 col-md-6">
-                    <input class="form-control mx-2" required name="name" placeholder="Nombre" />
+                    <input class="form-control form-control-lg mx-2" required name="name" placeholder="Nombre" />
                 </div>
                 <div class="col-12 col-md-6">
-                    <input class="form-control mx-2" required name="email" placeholder="Email" />
+                    <input class="form-control form-control-lg mx-2" required name="email" placeholder="Email" />
                 </div>
                 <div class="col-12">
                     <label class="form-label mt-3">Comentario: </label>
-                    <textarea class="form-control"></textarea>
+                    <textarea class="form-control form-control-lg"></textarea>
+                </div>
+                <div class="col-12">
+                    <button class="btn btn-primary rounded">Enviar</button>
                 </div>
             </div>
         </form>
