b7fec1208eac9335009bf3708fc4014762011f8c
derlisrd <derlisruizdiaz@hotmail.com>
2023-02-08 16:56:50 -0300

    visitas

--- app/Http/Controllers/RunnerController.php
+++ app/Http/Controllers/RunnerController.php
@@ -9,13 +9,14 @@ use Illuminate\Http\Request;
 class RunnerController extends Controller
 {
     public function run(){
-        $posts = Post::where('type','post')->get();
+        //$posts = Post::where('type','post')->get();
 
-        foreach($posts as $p){
+        /* foreach($posts as $p){
             PostsVisita::create([
                 'post_id'=>$p->id,
                 'visitas'=>0
             ]);
-        }
+        } */
+
     }
 }
